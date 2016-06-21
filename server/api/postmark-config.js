3879e2ead178468c11f1f37cf21092546c8809a4
Kevin Nguyen <account@kevinwin.com>
2016-06-21 01:51:40 -0700

    Clean up comments

--- server/api/postmark-config.js
+++ server/api/postmark-config.js
@@ -1,9 +1,3 @@
-// Send an email with the Postmark.js library 
-// Learn more -> http://developer.postmarkapp.com/developer-official-libs.html#node
-
-// Install with npm
-// npm install postmark --save
-
 var configPath = './../../.env';
 require('env2')(configPath);
 
@@ -15,15 +9,3 @@ var client = new postmark.Client(process.env.POSTMARK_SERVER_TOKEN);
 
 module.exports = client;
 
-// client.sendEmail({
-//     "From": "mail@kevinwin.com",
-//     "To": "mail@kevinwin.com",
-//     "Subject": "Hello World DOS!", 
-//     "TextBody": "Hello World from Postmark AGAIN WITH ENV VARIABLES!"
-// }, function(error, success) {
-//     if(error) {
-//         console.error("Unable to send via postmark: " + error.message);
-//         return;
-//     }
-//     console.info("Sent to postmark for delivery");
-// });
